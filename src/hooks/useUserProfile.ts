8cea3df8782c41ce19be8a5b11e2447667ba6599
taewonnn <thisis_tw94@naver.com>
2025-05-28 10:00:00 +0900

    feat: 유저 프로필 정보

---
+++ src/hooks/useUserProfile.ts
@@ -0,0 +1,22 @@
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../firebase/firebase';
+import { UserProfile as CustomUserProfile } from '../store/userStore';
+
+export const getUSerProfile = async (uid: string) => {
+  console.log('uid', uid);
+  try {
+    const userDoc = doc(db, 'users', uid);
+    const snapshot = await getDoc(userDoc);
+
+    if (snapshot.exists()) {
+      return snapshot.data() as CustomUserProfile;
+    }
+
+    return null;
+  } catch (e) {
+    console.log('유저 정보 조회 실패', e);
+    return null;
+  }
+};
+
+// export const updateUserProfile = (uid: string, updates: Partial<UserProfile>) => {};
