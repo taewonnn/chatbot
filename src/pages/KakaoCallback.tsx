060c5b40acfb7c1b76bbc38b302318d1f0e73b56
taewonnn <thisis_tw94@naver.com>
2025-07-05 10:00:00 +0900

    kakao

--- src/pages/KakaoCallback.tsx
+++ src/pages/KakaoCallback.tsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 export default function KakaoCallback() {
-  const url = useSearchParams(window.location.href);
-  console.log(url);
+  const [authCode, setAuthCode] = useState<string | null>(null);
+
+  useEffect(() => {
+    // 1. 인가코드 추출
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.get('code')) {
+      setAuthCode(urlParams.get('code') || null);
+    }
+    console.log('인가코드', authCode);
+  }, []);
+
   return <>kakao callback</>;
 }
