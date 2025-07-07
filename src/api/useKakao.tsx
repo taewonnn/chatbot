38fe77fa595a3b18968e03518c423a3703aff0bd
taewonnn <thisis_tw94@naver.com>
2025-07-07 10:00:00 +0900

    kakao

--- src/api/useKakao.tsx
+++ src/api/useKakao.tsx
@@ -1,5 +1,22 @@
-// import { http } from './common/http';
+import { useMutation } from 'react-query';
+import axios from 'axios';
 
-// const getKakaoToken = async (code: string) => {
-//   await
-// };
+const getKakaoToken = async (authCode: string) => {
+  const res = await axios.post('https://kauth.kakao.com/oauth/token', null, {
+    params: {
+      grant_type: 'authorization_code',
+      client_id: import.meta.env.VITE_KAKAO_REST_API_KEY, // 카카오 개발자 REST API KEY
+      redirect_uri: import.meta.env.VITE_KAKAO_REDIRECT_URI, // 인가 코드를 받은 리다이렉트 URI
+      code: authCode, // 카카오 로그인 시 발급해주는 인가 코드
+    },
+  });
+
+  // console
+  // console.log('토큰 받음?',res?.data);
+
+  return res?.data;
+};
+
+export const useGetKakaoToken = () => {
+  return useMutation({ mutationFn: getKakaoToken });
+};
