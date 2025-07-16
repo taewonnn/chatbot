e4475048fa6cde57106e0c9df37978178052573a
taewonnn <thisis_tw94@naver.com>
2025-07-16 10:00:00 +0900

    firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,17 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// import { onRequest } from 'firebase-functions/v2/https';
+// import * as logger from 'firebase-functions/logger';
+import { onCall } from 'firebase-functions/v2/https';
+import axios from 'axios';
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+/** 카카오 프로필 받기 */
+export const getKakaoProfile = onCall(async request => {
+  const { token } = request.data;
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+  const response = await axios.get('https://kapi.kakao.com/user/me', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+  return response.data;
+});
