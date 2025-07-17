e86a710962ff1aa879e1b56ca723a496fdf359aa
taewonnn <thisis_tw94@naver.com>
2025-07-17 13:40:30 +0900

    fix: firebase lint 설정

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -5,29 +5,29 @@ module.exports = {
     node: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'google',
-    'plugin:@typescript-eslint/recommended',
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "google",
+    "plugin:@typescript-eslint/recommended",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ['tsconfig.json'],
-    sourceType: 'module',
+    // project: ['tsconfig.json'],
+    sourceType: "module",
   },
   ignorePatterns: [
-    '/lib/**/*', // Ignore built files.
-    '/generated/**/*', // Ignore generated files.
-    '.eslintrc.js', //
+    "/lib/**/*", // Ignore built files.
+    "/generated/**/*", // Ignore generated files.
+    ".eslintrc.js", //
   ],
-  plugins: ['@typescript-eslint', 'import'],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
-    quotes: 'off', // 따옴표 규칙 비활성화
-    'object-curly-spacing': 'off', // 공백 규칙 비활성화
-    'arrow-parens': 'off', // 화살표 함수 괄호 규칙 비활성화
-    'import/no-unresolved': 0,
-    indent: ['error', 2],
+    quotes: "off", // 따옴표 규칙 비활성화
+    "object-curly-spacing": "off", // 공백 규칙 비활성화
+    "arrow-parens": "off", // 화살표 함수 괄호 규칙 비활성화
+    "import/no-unresolved": 0,
+    indent: ["error", 2],
   },
 };
