75de98d4b70ed54c724f7a510f1dd7f4c7975f5b
taewonnn <thisis_tw94@naver.com>
2025-07-20 10:00:00 +0900

    firebase - kakao profile

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,13 @@
 // import { onRequest } from 'firebase-functions/v2/https';
 // import * as logger from 'firebase-functions/logger';
-import { onCall } from 'firebase-functions/v2/https';
-import axios from 'axios';
+import {onCall} from "firebase-functions/v2/https";
+import axios from "axios";
 
 /** 카카오 프로필 받기 */
-export const getKakaoProfile = onCall(async request => {
-  const { token } = request.data;
+export const getKakaoProfile = onCall(async (request) => {
+  const {token} = request.data;
 
-  const response = await axios.get('https://kapi.kakao.com/user/me', {
+  const response = await axios.get("https://kapi.kakao.com/user/me", {
     headers: {
       Authorization: `Bearer ${token}`,
     },
