38fb340b198089d441e296792a6c6bb2d128c841
taewonnn <thisis_tw94@naver.com>
2025-07-30 11:18:38 +0900

    fix: vite 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     host: true,
     port: 3003,
     open: true,
-  }
-})
+  },
+  base: '/', // ✅ Vercel에서는 base: '/' 로 설정
+});
