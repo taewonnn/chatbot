fa32d25fba4e56ec5a7fd9c1319b3db94b2515fc
taewonnn <thisis_tw94@naver.com>
2025-08-07 10:30:33 +0900

    fix: prod 환경 콘솔 제거

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 3003,
-    open: true,
-  },
-  base: '/',
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      port: 3003,
+      open: true,
+    },
+    base: '/',
+    esbuild: {
+      drop: mode === 'production' ? ['console', 'debugger'] : [],
+    },
+    build: {
+      outDir: 'dist',
+      sourcemap: false,
+    },
+  };
 });
