189f1bb163b308603f93f601b46c1ead8355adb2
taewonnn <thisis_tw94@naver.com>
2025-08-11 15:33:02 +0900

    feat: vercel speed insight

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClientProvider, QueryClient } from 'react-query';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { router } from './router/router';
 import './index.css';
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      {process.env.NODE_ENV === 'production' && <SpeedInsights />}
     </QueryClientProvider>
   </React.StrictMode>,
 );
